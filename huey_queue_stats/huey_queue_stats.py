a4bf989da3de410cfb0b6952963749740f497e5a
Marco Glauser <marco.glauser@apgsga.ch>
2017-06-26 17:06:10 +0200

    added setup.py

--- huey_queue_stats/huey_queue_stats.py
+++ huey_queue_stats/huey_queue_stats.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 import os
-import re
 
 import click
 import redis
 import time
+
+from huey_queue_stats.queue import Queue
 
 
 @click.command()
@@ -38,28 +39,6 @@ def display_redis_stats(connection_string, queue_names, refresh_rate):
             print('\n\n')
 
         time.sleep(refresh_rate)
-
-
-class Queue:
-    length = 0
-    scheduled = 0
-    name = ''
-
-    def __init__(self, name, redis_connection):
-        self.name = name
-        self.clean_name = self.clean_name(name)
-        self.redis_queue = 'huey.redis.%s' % self.clean_name
-        self.redis_schedule = 'huey.schedule.%s' % self.clean_name
-        self.redis_connection = redis_connection
-        self.update()
-
-    def update(self):
-        self.length = self.redis_connection.llen(self.redis_queue)
-        self.scheduled = self.redis_connection.zcard(self.redis_schedule)
-
-    # Copied from https://github.com/coleifer/huey/blob/master/huey/storage.py#L139
-    def clean_name(self, name):
-        return re.sub('[^a-z0-9]', '', name)
 
 
 def main():
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(name='huey_queue_stats',
+      version='0.1.0',
+      description='Shows basic information about one or more huey queues',
+      author='Adtech @ APG SGA AG',
+      author_email='marco.glauser@apgsga.ch',
+      license='MIT',
+      packages=['huey_queue_stats'],
+      install_requires=required,
+      entry_points={
+          'console_scripts': [
+              'huey_queue_stats = huey_queue_stats.huey_queue_stats:main'
+          ]
+      }
+      )
