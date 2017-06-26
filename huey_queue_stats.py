94c6c229d8f88dd8b32c7f4f2d28143f19efd72c
Marco Glauser <marco.glauser@apgsga.ch>
2017-06-26 15:57:54 +0200

    some refactoring

--- huey_queue_stats.py
+++ huey_queue_stats.py
@@ -12,48 +12,54 @@ import time
               default='redis://localhost:6379',
               help='Connection string to redis including database. for example redis://localhost:6379/0'
               )
-@click.option('--queue', '-q', multiple=True, required=True, help='Name of the queue to print stats about. There can be multiple -q arguments.')
+@click.option('--queue_names', '-q', multiple=True, required=True, help='Name of the queues to print stats about. There can be multiple -q arguments.')
 @click.option('--refresh-rate', '-r', default=0.5, help='Stats refresh rate in seconds')
-def display_redis_stats(connection_string, queue, refresh_rate):
+def display_redis_stats(connection_string, queue_names, refresh_rate):
     os.system('clear')
     print('Connecting to redis...')
     r = redis.Redis.from_url(connection_string, max_connections=10)
-    queues = build_queue_dict(queue)
+    queues = []
+
+    for queue_name in queue_names:
+        queues.append(Queue(queue_name, r))
 
     while True:
         for queue in queues:
-            queue['length'] = r.llen(queue['redis_queue'])
-            queue['scheduled'] = r.zcard(queue['redis_schedule'])
+            queue.update()
 
         os.system('clear')
         for queue in queues:
             print('-'*40)
-            print(queue['name'] + ' - ' + queue['redis_queue'])
+            print(queue.name + ' - ' + queue.redis_queue)
             print('-'*40)
-            print('|' * min(100, queue['length']))
-            print('Queued: %s' % queue['length'])
-            print('Scheduled: %s' % queue['scheduled'])
+            print('|' * min(100, queue.length))
+            print('Queued: %s' % queue.length)
+            print('Scheduled: %s' % queue.scheduled)
             print('\n\n')
 
         time.sleep(refresh_rate)
 
 
-def build_queue_dict(queue_names):
-    queues = []
-    for queue_name in queue_names:
-        queue_dict = {
-            'name': queue_name,
-            'clean_name': clean_name(queue_name)
-        }
-        queue_dict['redis_queue'] = 'huey.redis.%s' % queue_dict['clean_name']
-        queue_dict['redis_schedule'] = 'huey.schedule.%s' % queue_dict['clean_name']
-        queues.append(queue_dict)
-    return queues
+class Queue:
+    length = 0
+    scheduled = 0
+    name = ''
 
+    def __init__(self, name, redis_connection):
+        self.name = name
+        self.clean_name = self.clean_name(name)
+        self.redis_queue = 'huey.redis.%s' % self.clean_name
+        self.redis_schedule = 'huey.schedule.%s' % self.clean_name
+        self.redis_connection = redis_connection
+        self.update()
 
-# Copied from https://github.com/coleifer/huey/blob/master/huey/storage.py#L139
-def clean_name(name):
-    return re.sub('[^a-z0-9]', '', name)
+    def update(self):
+        self.length = self.redis_connection.llen(self.redis_queue)
+        self.scheduled = self.redis_connection.zcard(self.redis_schedule)
+
+    # Copied from https://github.com/coleifer/huey/blob/master/huey/storage.py#L139
+    def clean_name(self, name):
+        return re.sub('[^a-z0-9]', '', name)
 
 
 def main():
