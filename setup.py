d2bc4794c400a5a46038d3648a6166122b8cd40c
Marco Glauser <marco.glauser@apgsga.ch>
2017-08-24 09:44:14 +0200

    Bump version: 0.2.1 → 0.3.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(name='huey_queue_stats',
-      version='0.2.1',
+      version='0.3.0',
       description='Shows basic information about one or more huey queues',
       author='Adtech @ APG SGA AG',
       author_email='marco.glauser@apgsga.ch',
